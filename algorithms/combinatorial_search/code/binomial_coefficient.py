3f12671a8cd0e8209642e0c0c35303bcc5c0beab
Anthony Ladson <aladsonjr@gmail.com>
2017-01-28 21:35:14 -0800

    remove print statements

--- algorithms/combinatorial_search/code/binomial_coefficient.py
+++ algorithms/combinatorial_search/code/binomial_coefficient.py
@@ -3,16 +3,10 @@ def compute_binomial_coefficient(n,k):
         return 1
     triangle = [1]
     for i in range(1, n+1):
-        # print "-"*10
         for j in reversed(range(1,i+1)):
-            # print i
-            # print j
             if j == i:
-                # print "here"
                 triangle.append(1)
             else:
-                # print len(triangle)
-                # print j
                 triangle[j] = triangle[j - 1] + triangle[j]
     return triangle[k]
 
